592976577934af0a7c510d0aa8795a454d020740
Giulia Napoli <gnapoli1996@gmail.com>
2020-11-02 17:43:06 +0100

    modifies css selector

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
-$('.header-right a').click(function (){
+$('.header-right > a').click(function (){
   $('.hamburger-menu').addClass('active');
 
 });
 
-$('.hamburger-menu a').click(function (){
+$('.hamburger-menu > a').click(function (){
   $('.hamburger-menu').removeClass('active');
 });
